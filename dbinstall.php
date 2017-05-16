be9042fb475bbdfc898d785b2de2e6a158baee22
ayan-majumder <ayan.majumder@innoraft.com>
2017-05-16 21:15:09 +0530

    modified dbinstall file

--- dbinstall.php
+++ dbinstall.php
@@ -1,11 +1,11 @@
 <?php
 include "variable_credentials.php";
-mysql_connect($servername,$username,$password); or die(mysql_error()); //change the credentials
+$dbcon=mysql_connect($servername,$username,$password) or die(mysql_error()); //change the credentials
 mysql_query("CREATE DATABASE IF NOT EXISTS ".$dbname."") or die(mysql_error());  //change db name
-mysql_select_db("twitter") or die(mysql_error());
+mysql_select_db($dbname,$dbcon) or die(mysql_error());
 mysql_query("CREATE TABLE IF NOT EXISTS users(user_id TEXT NOT NULL, t_count INT(2) NOT NULL)") or die(mysql_error()); //change the table names
-mysql_query("CREATE TABLE IF NOT EXISTS donated_items(item_id INT(11) NOT NULL, name TEXT NOT NULL, description TEXT, donated INT(11) DEFAULT (0), image VARCHAR(255) NOT NULL)") or die(mysql_error());
-mysql_query("CREATE TABLE IF NOT EXISTS lasttweet(id INT(2) NOT NULL AUTO_INCREMENT, last_id TEXT,)") or die(mysql_error());
+mysql_query("CREATE TABLE IF NOT EXISTS donated_items(item_id INT(11) NOT NULL, name TEXT NOT NULL, description TEXT, donated INT(11) DEFAULT 0, image VARCHAR(255) NOT NULL)") or die(mysql_error());
+mysql_query("CREATE TABLE IF NOT EXISTS lasttweet(id INT(2) NOT NULL AUTO_INCREMENT, last_id TEXT, PRIMARY KEY(id))") or die(mysql_error());
 mysql_query("CREATE TABLE IF NOT EXISTS admin(email_id VARCHAR(255) NOT NULL, password VARCHAR(50) NOT NULL, PRIMARY KEY(email_id))") or die(mysql_error());
 // header("Location: index.php");
 ?>
