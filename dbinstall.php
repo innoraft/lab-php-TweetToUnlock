42a90810bc8e17fc8d7f04e08f0b1b866e3b7286
ayan-majumder <ayan.majumder@innoraft.com>
2017-05-17 16:51:18 +0530

    added admin creds

--- dbinstall.php
+++ dbinstall.php
@@ -7,5 +7,6 @@ mysql_query("CREATE TABLE IF NOT EXISTS users(user_id TEXT NOT NULL, t_count INT
 mysql_query("CREATE TABLE IF NOT EXISTS donated_items(item_id INT(11) NOT NULL, name TEXT NOT NULL, description TEXT, donated INT(11) DEFAULT 0, image VARCHAR(255) NOT NULL)") or die(mysql_error());
 mysql_query("CREATE TABLE IF NOT EXISTS lasttweet(id INT(2) NOT NULL AUTO_INCREMENT, last_id TEXT, PRIMARY KEY(id))") or die(mysql_error());
 mysql_query("CREATE TABLE IF NOT EXISTS admin(email_id VARCHAR(255) NOT NULL, password VARCHAR(50) NOT NULL, PRIMARY KEY(email_id))") or die(mysql_error());
-// header("Location: index.php");
+mysql_query("INSERT INTO admin(email_id,password) VALUES('admin@admin.com','admin123')") or die(mysql_error());
+header("Location: login.php");
 ?>
